677036e476be4c1a942137ca739dd8f2a86165cd
Manikanta <manikantasidaga@gmail.com>
2024-08-17 23:11:15 +0530

    connect with us modal updated and got form data need to send that data to backend is issue

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,33 +2,50 @@ import { Component, Inject, OnInit, PLATFORM_ID, Renderer2 } from '@angular/core
 import bootstrap from '../../main.server';
 
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   formSubmitted = false;
+  formData = {
+    clientName: '',
+    clientEmail: '',
+    clientAddress: ''
+  };
 
-  onFormSubmit(event: Event) {
-    event.preventDefault();
-    // Form submission logic here
+  onFormSubmit() {
+    this.formSubmitted = true;
+    console.log(this.formData)
+    
 
-    // Simulate form submission
-    setTimeout(() => {
-      this.formSubmitted = true;
-    }, 500); // Simulate network delay
-
-    // Hide the success message after 5 seconds
-    setTimeout(() => {
-      this.formSubmitted = false;
-    }, 5000); // Adjust the time as needed
   }
 
   closeSuccessMessage() {
+    this.formData = {
+      clientName: '',
+      clientEmail: '',
+      clientAddress: ''
+    };
     this.formSubmitted = false;
+
+    // Optionally, close the modal
+    const modal = document.getElementById('connectModal');
+    if (modal) {
+      modal.classList.remove('show');
+      modal.setAttribute('aria-hidden', 'true');
+      modal.removeAttribute('aria-modal');
+      modal.style.display = 'none';
+
+      const modalBackdrop = document.querySelector('.modal-backdrop');
+      if (modalBackdrop) {
+        modalBackdrop.remove();
+      }
+    }
   }
 }
