c4513b9c8dccae463a7c0fe7c1796d67938dbbba
Manikanta <manikantasidaga@gmail.com>
2024-08-30 13:35:47 +0530

    session problem cleared

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,19 +6,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  private auth = 'https://service1-sjlm.onrender.com/api/user';
-  // private auth='http://localhost:8090/api/user';
+  // private auth = 'https://service1-sjlm.onrender.com/api/user';
+  private auth='http://localhost:8090/api/user';
 
 
   constructor(private http: HttpClient) {}
   mobileNumber :number |null=null
 
   checkUser(mobileNumber: number): Observable<boolean> {
-    return this.http.get<boolean>(`${this.auth}/auth?mobileNumber=${mobileNumber}`);
+    return this.http.get<boolean>(`${this.auth}/auth?mobileNumber=${mobileNumber}`,{ withCredentials: true });
   }
 
   getCurrentUser():Observable<number>{
-    return this.http.get<number>(`${this.auth}/currentUser`)
+    return this.http.get<number>(`${this.auth}/currentUser`, { withCredentials: true })
   }
 
 
