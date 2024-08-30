87335935346e8bba343dde1fd31a3ed889d1c2cd
Manikanta <manikantasidaga@gmail.com>
2024-08-30 13:37:07 +0530

    session retrieving problem cleared

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  // private auth = 'https://service1-sjlm.onrender.com/api/user';
-  private auth='http://localhost:8090/api/user';
+  private auth = 'https://service1-sjlm.onrender.com/api/user';
+  // private auth='http://localhost:8090/api/user';
 
 
   constructor(private http: HttpClient) {}
